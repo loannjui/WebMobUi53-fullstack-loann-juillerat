1dd3404c7b9b3b4bea05b58411a9d903ad30acf7
loannjui <loann.juillerat@heig-vd.ch>
2026-05-17 20:18:09 +0200

    Changements seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -146,7 +146,7 @@ class DatabaseSeeder extends Seeder
                 DB::table('polls')->insert([
                     'id' => 1,
                     'user_id' => 1,
-                    'title' => 'test',
+                    'title' => 'test John',
                     'question' => 'Quelle est votre réponse ?',
                     'secret_token' => \Illuminate\Support\Str::random(32),
                     'is_draft' => false,
@@ -172,7 +172,7 @@ class DatabaseSeeder extends Seeder
                 DB::table('polls')->insert([
                     'id' => 2,
                     'user_id' => 2,
-                    'title' => 'test',
+                    'title' => 'test Jane',
                     'question' => 'Quelle est votre réponse ?',
                     'secret_token' => \Illuminate\Support\Str::random(32),
                     'is_draft' => false,
